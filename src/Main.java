64afe9190b2e0de80b795b19cc6f8cbcce5e64b4
DouORd3n <DouORd3n@gmail.com>
2021-03-01 20:12:48 +0200

    12

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ personList.forEach(p-> System.out.println(p));
         Person temp;
         for (int i = 0; i <list.size()-1 ; i++) {
             for (int j = 0; j <list.size()-1 ; j++) {
-                if (list.get(j).hashCode()>list.get(j+1).hashCode()){
+                if (list.get(j).getAge()>list.get(j+1).getAge()){
                     temp = list.get(j+1);
                     list.set(j+1,list.get(j));
                     list.set(j, temp);
